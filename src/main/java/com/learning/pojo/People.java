15ca0b1c43c7485835bce62b1e9668f1c6e111de
xavier <xavier_tang@163.com>
2020-04-01 08:52:59 +0800

    add pojo

---
+++ src/main/java/com/learning/pojo/People.java
@@ -0,0 +1,14 @@
+package com.learning.pojo;
+
+import lombok.Data;
+
+/**
+ * @author tzwei
+ * @create 2020/4/1
+ * @since 1.0.0
+ */
+@Data
+public class People {
+    private String name;
+    private String age;
+}
---
+++ src/main/java/com/learning/pojo/base/CommonUtil.java
@@ -0,0 +1,10 @@
+package com.learning.pojo.base;
+
+/**
+ * @author tzwei
+ * @create 2020/4/1
+ * @since 1.0.0
+ */
+public class CommonUtil {
+    private String name;
+}
